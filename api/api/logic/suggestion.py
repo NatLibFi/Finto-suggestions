d9dcac22a34389ce2ea7044c5c60ef8fd2c3b65a
Peter Kronström <peter.kronstrom@gofore.com>
2018-06-26 14:29:24 +0300

    Modify api schema

--- api/api/logic/suggestion.py
+++ api/api/logic/suggestion.py
@@ -1,34 +1,40 @@
+import connexion
+
+DUMMY_SUGGESTION = {'id': 1000,
+                    'uri': 'http://www.google.fi',
+                    'prefLabel': ['Kissa @fi', 'Katt @sv', 'Cat @en'],
+                    'type': 0,
+                    'status': 3
+                    }
 
 
 def get_suggestions(limit: int = None, offset: int = None) -> str:
     return [
-        {'id': 1000,
-         'uri': 'http://www.google.fi',
-         'prefLabel': ['Kissa @fi', 'Katt @sv', 'Cat @en'],
-         'type': 0,
-         'status': 3
-         }
+        DUMMY_SUGGESTION
     ], 200
 
 
 def post_suggestion() -> str:
-    return "success!", 201
+    body = connexion.request.json  # usually the post payload is returned
+    return body, 201
     # TODO: failed status code missing
 
 
 def get_suggestion(suggestion_id: int) -> str:
-    return "success get", 200
+    return DUMMY_SUGGESTION, 200
     # return "failed", 404
 
 
 def put_suggestion(suggestion_id: int) -> str:
-    return "success put", 200
+    return DUMMY_SUGGESTION, 204
     # return "failed", 404
 
 
 def patch_suggestion(suggestion_id: int) -> str:
-    # TODO: requires a schema for object body
-    return "success patch", 200
+    body = connexion.request.json
+    patched = dict(DUMMY_SUGGESTION).update(body)
+
+    return patched, 200
     # return "patch failed", 404
 
 
