c7a3d397b069df1e1c93c914b5f4f44431ce44ac
Peter Kronström <peter.kronstrom@gofore.com>
2018-07-04 14:06:12 +0300

    Add migration scripts
    - Use flask-migrate for database migrations
    - Remove init_db.py script, since migrations will be used for initialization, also
    - Add a bunch of helper scripts to Pipfile scripts

--- api/app.py
+++ api/app.py
@@ -1,26 +1,26 @@
 import connexion
 import os
 from api.models import db
+from flask_migrate import Migrate
 
-# Create a WSGI compliant app instance
+# Create a WSGI compliant Connexion app instance and initialize the API
 app = connexion.FlaskApp(__name__, specification_dir='api/specification/')
+app.add_api('suggestion.yaml', base_path="/api/v1")
 
 # This is required for Flask cli and FLASK_APP env to work
 flask_app = app.app
 
 # Add PSQL database config for SQLAlchemy
 flask_app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI')
-
-# http://flask-sqlalchemy.pocoo.org/2.1/signals/
+# Disable signals. http://flask-sqlalchemy.pocoo.org/2.1/signals/
 flask_app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 flask_app.secret_key = os.environ.get('APP_SECRET_KEY')
 
-# Init database models
+# Init database models and migrations
 db.init_app(flask_app)
+migrate = Migrate(flask_app, db)
 
-# Add Connexion API endpoints
-app.add_api('suggestion.yaml', base_path="/api/v1")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8050, debug=True)
--- api/init_db.py
+++
@@ -1,6 +0,0 @@
-from app import flask_app, db
-
-with flask_app.app_context():
-    db.create_all()
-
-print("Database initialized.")
