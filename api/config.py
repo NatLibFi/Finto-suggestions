9164f7bba366315ddeefc3a44817dcd1b3ca8eca
Peter Kronström <peter.kronstrom@gofore.com>
2018-08-15 15:24:43 +0300

    Update docker-compose to use .env file

--- api/config.py
+++ api/config.py
@@ -1,11 +1,17 @@
+import os
+
+user = os.environ.get('POSTGRES_USER')
+pw = os.environ.get('POSTGRES_PASSWORD')
+db = os.environ.get('POSTGRES_DB')
+
 
 class BaseConfig:
     """
     Default values for app configurations. Do not modify these.
     Rather, overwrite the DevelopmentConfig and ProductionConfig values.
     """
-
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://psql:pw@db:5432/psdb'
+    # postgresql+psycopg2://psql:pw@db:5432/psdb
+    SQLALCHEMY_DATABASE_URI = f'postgresql+psycopg2://{user}:{pw}@db:5432/{db}'
 
     # Disable signals. http://flask-sqlalchemy.pocoo.org/2.1/signals/
     SQLALCHEMY_TRACK_MODIFICATIONS = False
@@ -31,7 +37,7 @@ class ProductionConfig(BaseConfig):
 
 class TestingConfig(BaseConfig):
     # testdb db will be created upon test initialization
-    SQLALCHEMY_DATABASE_ROOT = 'postgres://psql:pw@db:5432/'
+    SQLALCHEMY_DATABASE_ROOT = f'postgres://psql:{pw}@{user}:5432/'
     SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_ROOT + 'testdb'
     ENABLE_SWAGGER_UI = False
     DEBUG = True
