9b227c44fc746c4b99be18d434e7edf8e49d0afc
Ville Hytönen <ville.hytonen@gofore.com>
2018-09-19 15:42:57 +0300

    feat: added frames for search base component and child components

---
+++ web/src/components/suggestion/SuggestionSearchForm.vue
@@ -0,0 +1,21 @@
+<template>
+  <div :class="searchSuggestions">
+    <h4>Hae ehdotusta</h4>
+    <span>
+      <form>
+      <input type="text" value="Hae jotain" />
+          <input type="button" value="Hae" />
+        </form>
+      </span>
+    </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style scoped>
+
+</style>
--- web/src/views/Index.vue
+++ web/src/views/Index.vue
@@ -1,17 +1,20 @@
 <template>
   <div>
     <navigation></navigation>
+    <SuggestionSearch />
     <suggestion-list></suggestion-list>
   </div>
 </template>
 
 <script>
 import Navigation from '../components/navigation/Navigation';
+import SuggestionSearch from '../components/suggestion/SuggestionList';
 import SuggestionList from '../components/suggestion/SuggestionList';
 
 export default {
   components: {
     Navigation,
+    SuggestionSearch,
     SuggestionList
   }
 };
