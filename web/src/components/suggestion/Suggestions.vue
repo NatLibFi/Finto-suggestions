7ceadabf467ae8e63c8eb456e32d0dbc0525245d
Ville Hytönen <ville.hytonen@gofore.com>
2018-09-19 14:39:50 +0300

    feature: vuex state management for api calls and some minor refactoring

---
+++ web/src/components/suggestion/Suggestions.vue
@@ -0,0 +1,89 @@
+<template>
+<div>
+  <suggestion-header
+    :openSuggestionCount="openCount"
+    :resolvedSuggestionCount="resolvedCount"
+    @sortSuggestionListBy="sortSuggestionList"
+  />
+  <ul class="list">
+    <suggestion-item
+      class="item"
+      v-for="item in items"
+      :key="item.id"
+      :orderNumber=item.id
+      :title=item.description
+      :created=item.created
+      :status=item.status
+      :suggestionType=item.suggestion_type
+      :tags=item.tags />
+  </ul>
+</div>
+</template>
+
+<script>
+import SuggestionHeader from './SuggestionHeader';
+import SuggestionItem from './SuggestionItem';
+
+import {
+  suggestionGetters,
+  suggestionActions
+} from '../../store/modules/suggestionConsts.js';
+
+import {
+  mapSuggestionActions,
+  mapSuggestionGetters
+} from '../../store/modules/suggestion.js';
+
+export default {
+  components: {
+    SuggestionHeader,
+    SuggestionItem
+  },
+  computed: {
+    ...mapSuggestionGetters({
+      items: suggestionGetters.GET_SUGGESTIONS,
+      openCount: suggestionGetters.GET_OPEN_SUGGESTIONS_COUNT,
+      resolvedCount: suggestionGetters.GET_RESOLVED_SUGGESTIONS_COUNT
+    })
+  },
+  created() {
+    this.getSuggestions();
+    this.getOpenSuggestionCount();
+    this.getResolvedSuggestionCount();
+  },
+  methods: {
+    ...mapSuggestionActions({
+      getSuggestions: suggestionActions.GET_SUGGESTIONS,
+      getOpenSuggestionCount: suggestionActions.GET_OPEN_SUGGESTIONS,
+      getResolvedSuggestionCount: suggestionActions.GET_RESOLVED_SUGGESTIONS,
+      getSortedSuggestions: suggestionActions.GET_SORTED_SUGGESTIONS
+    }),
+    async sortSuggestionList(sortValue) {
+      console.log(sortValue);
+      if (sortValue !== '') {
+        this.getSortedSuggestions(sortValue);
+      } else {
+        this.getSuggestions();
+      }
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+ul {
+  list-style: none;
+}
+
+.list {
+  align-items: flex-start;
+  text-align: start;
+}
+
+.item {
+  margin: 10px 0 10px 0;
+  border-top: 2px solid #b1bfd6;
+  border-bottom: 2px solid #b1bfd6;
+}
+</style>
