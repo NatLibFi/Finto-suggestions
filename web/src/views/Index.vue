8de395d614b2090a3acb6a0f52337be2804d89b8
Ville Hytönen <ville.hytonen@gofore.com>
2018-09-19 15:42:57 +0300

    feat: added frames for search base component and child components

--- web/src/views/Index.vue
+++ web/src/views/Index.vue
@@ -1,17 +1,25 @@
 <template>
+<<<<<<< HEAD
   <div>
     <navigation></navigation>
+=======
+  <div class="container">
+    <!-- <img src="../assets/logo.png"> -->
+    <SuggestionSearch />
+>>>>>>> feat: added frames for search base component and child components
     <Suggestions />
   </div>
 </template>
 
 <script>
 import Navigation from '../components/navigation/Navigation';
+import SuggestionSearch from '../components/suggestion/SuggestionSearch';
 import Suggestions from '../components/suggestion/Suggestions';
 
 export default {
   components: {
     Navigation,
+    SuggestionSearch,
     Suggestions
   }
 };
