c70802ce20d5db6352132ac370dea366d887271b
Ville Hytönen <ville.hytonen@gofore.com>
2018-09-20 10:38:30 +0300

    feat: dosearch binding when typing something and pressin hae-button

--- web/src/components/suggestion/SuggestionSearch.vue
+++ web/src/components/suggestion/SuggestionSearch.vue
@@ -1,7 +1,7 @@
 <template>
-  <div :class="container">
+  <div class="container">
     <h3>YSE - YSAn ja YSOn käsite-ehdotukset</h3>
-    <div :class="welcomeText">
+    <div class="welcomeText">
       <p>
         Tämä palvelu on YSA/YSO -käsite-ehdotuksille ja niiden etenemisen seuraamiselle.
         Voit selata ja kommentoida ehdotuksia täällä. Ehdotusten selaaminen onnistuu ilman käyttäjätunnuksia,
@@ -10,35 +10,9 @@
         Uusien käsite-ehdotuksien tekeminen onnistuu Fintossa.
       </p>
     </div>
-    <!-- <div :class="searchSuggestions">
-      <h4>Hae ehdotusta</h4>
-      <span>
-        <form>
-          <input type="text" value="Hae jotain" />
-          <input type="button" value="Hae" />
-        </form>
-      </span>
-    </div> -->
     <div>
       <suggestion-search-form />
     </div>
-    <!-- <div :class="filterSuggestions">
-      <h4>Suodata hakutuloksia</h4>
-      <span>
-        <select>
-          <option>Käsittelyn tila</option>
-        </select>
-        <select>
-          <option>Ehdotustyyppi</option>
-        </select>
-        <select>
-          <option>Tyyppi</option>
-        </select>
-        <select>
-          <option>Kokous</option>
-        </select>
-      </span>
-    </div> -->
     <div>
       <filter-suggestions />
     </div>
@@ -58,5 +32,6 @@ export default {
 </script>
 
 <style scoped>
-
+.container {}
+.welcomeText {}
 </style>
