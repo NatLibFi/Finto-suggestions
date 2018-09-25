75db7c96b71afa3d9db7d182714f9ced989ecb8e
Ville Hytönen <ville.hytonen@gofore.com>
2018-09-25 12:34:38 +0300

    feat: vuex store addons for tags, and calling in filtersuggestion file

---
+++ web/src/store/modules/tag/tagConst.js
@@ -0,0 +1,17 @@
+export const namespace = 'meeting';
+
+export const storeStateNames = {
+  ITEMS: 'items'
+};
+
+export const tagMutations = {
+  SET_TAGS: 'setTags'
+};
+
+export const tagGetters = {
+  GET_TAGS: 'getTags'
+};
+
+export const tagActions = {
+  GET_TAGS: 'getTags'
+};
