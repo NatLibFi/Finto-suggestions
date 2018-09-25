730fa6ac32eda95e9b56dedcb3f35f46e19af70d
Ville Hytönen <ville.hytonen@gofore.com>
2018-09-25 11:53:03 +0300

    feat: vuex store module for events and api gateway for events

---
+++ web/src/store/modules/meeting/meetingConst.js
@@ -0,0 +1,17 @@
+export const namespace = 'meeting';
+
+export const storeStateNames = {
+  ITEMS: 'items'
+};
+
+export const meetingMutations = {
+  SET_MEETINGS: 'setMeetings'
+};
+
+export const meetingGetters = {
+  GET_MEETINGS: 'getMeetings'
+};
+
+export const meetingActions = {
+  GET_MEETINGS: 'getMeetings'
+};
