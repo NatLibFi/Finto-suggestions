dc98686fe188dc15f8cc067c3cdb56a0f19168ea
Ville Hytönen <ville.hytonen@gofore.com>
2018-10-08 09:17:04 +0300

    feat: vue-cookies dependency added, it will save from env-file token to cookies meanwhile when login is ready

--- web/src/App.vue
+++ web/src/App.vue
@@ -7,6 +7,16 @@
   </div>
 </template>
 
+<script>
+export default {
+  created() {
+    $cookies.config('10d');
+    $cookies.set('access_token', process.env.ACCESS_TOKEN);
+  }
+};
+</script>
+
+
 <style>
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
