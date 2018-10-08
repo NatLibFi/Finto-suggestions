7897533fae343ea84b410ec538f316e03f061911
Ville Hytönen <ville.hytonen@gofore.com>
2018-10-08 09:17:04 +0300

    feat: vue-cookies dependency added, it will save from env-file token to cookies meanwhile when login is ready

--- web/src/main.js
+++ web/src/main.js
@@ -1,4 +1,5 @@
 import Vue from 'vue';
+import cookies from 'vue-cookies';
 import App from './App.vue';
 import router from './router/index';
 import store from './store/index';
@@ -8,5 +9,6 @@ Vue.config.productionTip = false;
 new Vue({
   router,
   store,
+  cookies,
   render: h => h(App)
 }).$mount('#app');
