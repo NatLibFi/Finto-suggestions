9d6dce09dec1d5dafd8ca89b3597df91c8728060
Ville Hytönen <ville.hytonen@gofore.com>
2018-10-08 09:19:54 +0300

    feat: added meeting and tag modules to store

--- web/src/store/index.js
+++ web/src/store/index.js
@@ -2,11 +2,15 @@ import Vue from 'vue';
 import Vuex from 'vuex';
 
 import suggestionModule from './modules/suggestion/suggestionModule';
+import meetingModule from './modules/meeting/meetingModule';
+import tagModule from './modules/tag/tagModule';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   modules: {
-    suggestion: suggestionModule
+    suggestion: suggestionModule,
+    meeting: meetingModule,
+    tag: tagModule
   }
 });
