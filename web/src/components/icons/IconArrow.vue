0f1b439ca52702ee9786e6ee0be31a29cc97decf
Miki Pernu <mikael.pernu@gmail.com>
2018-10-11 08:39:49 +0300

    feat: add component for single Suggestion and listing its details; icon for back button

---
+++ web/src/components/icons/IconArrow.vue
@@ -0,0 +1,6 @@
+<template>
+  <g>
+    <path d="M11.5 3L1.5 3" stroke="currentColor" stroke-linecap="round"/>
+    <path d="M4.5 5L1 3L4.5 1" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"/>
+  </g>
+</template>
