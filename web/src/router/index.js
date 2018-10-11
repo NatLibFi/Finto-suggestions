dd4812f347f60a115f62405e75257a541bd978ee
Miki Pernu <mikael.pernu@gmail.com>
2018-10-11 08:41:37 +0300

    feat: add View for single suggestion; add routing and links to Suggestion view

--- web/src/router/index.js
+++ web/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Index from '../views/Index.vue';
+import Suggestion from '../views/Suggestion.vue';
 
 Vue.use(Router);
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'index',
       component: Index
+    },
+    {
+      path: '/suggestion/:suggestionID',
+      name: 'suggestion',
+      component: Suggestion
     }
   ],
   mode: 'history'
