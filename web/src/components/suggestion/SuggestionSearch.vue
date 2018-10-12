a498e0b0ab5877bc9262907684f7184d226cedd2
Ville Hytönen <ville.hytonen@gofore.com>
2018-10-12 17:07:37 +0300

    fix: fixed suggestionsearch data must be function

--- web/src/components/suggestion/SuggestionSearch.vue
+++ web/src/components/suggestion/SuggestionSearch.vue
@@ -22,15 +22,15 @@ export default {
     SuggestionSearchForm,
     FilterSuggestions
   },
-  data: function() {
-    welcomeText:
+  data: () => ({
+      welcomeText:
       `Tämä palvelu on YSA/YSO -käsite-ehdotuksille ja niiden etenemisen seuraamiselle.
       Voit selata ja kommentoida ehdotuksia täällä. Ehdotusten selaaminen onnistuu ilman
       käyttäjätunnuksia,mutta kommentointi edellyttää tunnuksen luomista. Voit osoittaa
       tukesi ehdotukselle kommentoimalla sitä. Halutessasi voit nopeuttaa ehdotuksen
       käsittelyä jättämällä ehdotuksen kommenttikenttään täsmennyksiä ja lisätietoja.
-      Uusien käsite-ehdotuksien tekeminen onnistuu Fintossa.`;
-  }
+      Uusien käsite-ehdotuksien tekeminen onnistuu Fintossa.`
+  })
 };
 </script>
 
