b6ed684f260c732580b07b8887480d5283a767c2
Ville Hytönen <ville.hytonen@gofore.com>
2018-11-26 12:18:03 +0200

    refactor: refactoring a bit front cookie handles and other things

--- web/src/main.js
+++ web/src/main.js
@@ -1,13 +1,10 @@
 import Vue from 'vue';
-import VueCookies from 'vue-cookies';
 
 import App from './App.vue';
 import router from './router/index';
 import store from './store/index';
 
 Vue.config.productionTip = false;
-
-Vue.use(VueCookies);
 
 new Vue({
   router,
