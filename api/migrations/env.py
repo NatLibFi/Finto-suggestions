ec31e9d29b59cde9996f996f6c4c99c94dc3fd58
Ville Hytönen <ville.hytonen@gofore.com>
2018-12-03 17:15:46 +0200

    fix: drone build not launching :(

--- api/migrations/env.py
+++ api/migrations/env.py
@@ -18,7 +18,9 @@ logger = logging.getLogger('alembic.env')
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
 from flask import current_app
+
 print(current_app.config.get('SQLALCHEMY_DATABASE_URI'))
+
 config.set_main_option('sqlalchemy.url',
                        current_app.config.get('SQLALCHEMY_DATABASE_URI'))
 target_metadata = current_app.extensions['migrate'].db.metadata
