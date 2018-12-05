a076cfdae9df27a079300db57c5d224bce1bfc21
Viridiana Stålhammar <mikael.pernu@gmail.com>
2018-12-05 07:19:15 +0200

    feat: Add suggestion listing for specific meeting

---
+++ web/src/views/MeetingSuggestionList.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <navigation />
+    <meeting-suggestion-list :meetingId="meetingId" />
+  </div>
+</template>
+
+<script>
+import Navigation from '../components/navigation/Navigation';
+import MeetingSuggestionList from '../components/meetings/MeetingSuggestionList';
+
+export default {
+  components: {
+    Navigation,
+    MeetingSuggestionList
+  },
+  props: {
+    meetingId: {
+      type: [String, Number],
+      required: true
+    }
+  }
+};
+</script>
