0afaaf043f498664cdc9e0f2f8c7a339d22f3d90
Ville Hytönen <ville.hytonen@gofore.com>
2018-12-11 12:25:06 +0200

    fix: refactore userNameInitials to get parameter in and return initals by capitalized and fixed in navigation component

--- web/src/utils/nameHelpers.js
+++ web/src/utils/nameHelpers.js
@@ -1,12 +1,13 @@
-export const userNameInitials = () => {
-  if (this.userName && this.userName.length > 0) {
+export const userNameInitials = userName => {
+  if (userName && userName.length > 0) {
     let initials = '';
-    const splitArray = this.userName.split(' ');
+    const splitArray = userName.split(' ');
     if (splitArray && splitArray.length > 0) {
       const firstElement = splitArray[0];
       const lastElement = splitArray.slice(-1)[0];
       initials = `${firstElement[0].toUpperCase()}${lastElement[0].toUpperCase()}`;
-      this.userInitials = initials;
+      return initials;
     }
   }
+  return '';
 };
