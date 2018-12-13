735ed67f58d6c3e5aa731ea456c4e97f2271716e
Ville Hytönen <ville.hytonen@gofore.com>
2018-12-13 10:01:09 +0200

    feature: fetching meeting past and future count from database and updates to component

--- web/src/store/modules/meeting/meetingConst.js
+++ web/src/store/modules/meeting/meetingConst.js
@@ -1,17 +1,24 @@
 export const namespace = 'meeting';
 
 export const storeStateNames = {
-  ITEMS: 'items'
+  ITEMS: 'items',
+  FUTURE_MEETINGS_COUNT: 'futureMeetings',
+  PAST_MEETINGS_COUNT: 'pastMeetings'
 };
 
 export const meetingGetters = {
-  GET_MEETINGS: 'getMeetings'
+  GET_MEETINGS: 'getMeetings',
+  GET_FUTURE_MEETINGS_COUNT: 'getFutureMeetings',
+  GET_PAST_MEETINGS_COUNT: 'getPastMeetings'
 };
 
 export const meetingMutations = {
-  SET_MEETINGS: 'setMeetings'
+  SET_MEETINGS: 'setMeetings',
+  SET_FUTURE_MEETINGS_COUNT: 'setFutureMeetings',
+  SET_PAST_MEETINGS_COUNT: 'setPastMeetings'
 };
 
 export const meetingActions = {
-  GET_MEETINGS: 'getMeetings'
+  GET_MEETINGS: 'getMeetings',
+  GET_FUTURE_AND_PAST_MEETINGS_COUNT: 'getFutureAndPastMeetingsCount'
 };
