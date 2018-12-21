cd41db6f3226d2d609a0a71e5946531074ff56e5
Ville Hytönen <ville.hytonen@gofore.com>
2018-12-21 13:33:50 +0200

    fix: disabled add new comment field if user is not logged in

--- web/src/components/suggestion/AddComment.vue
+++ web/src/components/suggestion/AddComment.vue
@@ -7,9 +7,9 @@
       </div>
     </div>
     <div class="comment-box">
-      <markdown-editor v-model="content" ref="markdownEditor"></markdown-editor>
+      <markdown-editor v-model="content" ref="markdownEditor" :class="[!isAuthenticated ? 'disabled' : '']"></markdown-editor>
     </div>
-    <div class="comment-submit">
+    <div class="comment-submit" v-if="isAuthenticated">
       <span @click="saveNewComment" class="submit-button">
         Lähetä kommentti
       </span>
@@ -43,14 +43,17 @@ export default {
     content: ''
   }),
   computed: {
-    ...mapAuthenticatedUserGetters({ userId: authenticatedUserGetters.GET_USER_ID })
+    ...mapAuthenticatedUserGetters({
+      userId: authenticatedUserGetters.GET_USER_ID,
+      isAuthenticated: authenticatedUserGetters.GET_AUTHENTICATE
+    })
   },
   methods: {
     ...mapEventActions({
       addNewEvent: eventActions.ADD_NEW_EVENT
     }),
     constructEventJsonObject() {
-      if (this.userId > 0 && this.suggestionId > 0) {
+      if (this.isAuthenticated && this.userId > 0 && this.suggestionId > 0) {
         return {
           event_type: eventTypes.COMMENT,
           text: this.content,
@@ -127,6 +130,14 @@ div.comment-submit {
 
 .submit-button:hover {
   background-color: #44bdb2;
+}
+
+div.disabled
+{
+  pointer-events: none;
+  /* for "disabled" effect */
+  opacity: 0.5;
+  background: #CCC;
 }
 
 @media (max-width: 750px) {
