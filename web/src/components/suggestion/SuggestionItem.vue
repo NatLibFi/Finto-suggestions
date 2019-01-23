0439ff89c03c43781997f7d890f9a1c8fcd44e49
Miki Pernu <mikael.pernu@gmail.com>
2019-01-23 12:27:06 +0200

    refactor: remove unnecessary console.log

--- web/src/components/suggestion/SuggestionItem.vue
+++ web/src/components/suggestion/SuggestionItem.vue
@@ -65,7 +65,6 @@ export default {
   methods: {
     goToSuggestion() {
       if (!this.meetingId) {
-        console.log('asdasd', this.meetingId);
         this.$router.push({
           name: 'suggestion',
           params: {
