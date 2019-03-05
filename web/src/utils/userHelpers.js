7d3765e02f3b193caac8e1ca8963e60b779bc312
Miki Pernu <mikael.pernu@gmail.com>
2019-03-05 19:45:27 +0200

    feat: add option for one user initial in user-bubble and refactor huge function

--- web/src/utils/userHelpers.js
+++ web/src/utils/userHelpers.js
@@ -5,16 +5,48 @@ export const userRoles = {
 
 export const userNameInitials = userName => {
   let initials = '';
-  if (userName && userName.length > 0) {
+  if (existsAndHasLength(userName)) {
     const splitArray = userName.split(' ');
-    if (splitArray && splitArray.length > 0) {
-      const firstElement = splitArray[0];
-      const lastElement = splitArray[1];
-      if (firstElement && lastElement && (firstElement.length > 0 && lastElement.length > 0)) {
-        initials = `${firstElement[0].toUpperCase()}${lastElement[0].toUpperCase()}`;
-        return initials;
-      }
+    if (existsAndHasLength(splitArray)) {
+      return createInitials(splitArray);
     }
   }
   return initials;
+};
+
+const createInitials = array => {
+  if (hasOnlyOneValidElement(array)) {
+    return createOneInitial(array);
+  }
+  if (hasTwoValidElements(array)) {
+    return createTwoInitials(array);
+  }
+};
+
+const createOneInitial = array => {
+  const string = array[0];
+  const initial = `${string[0].toUpperCase()}`;
+  return initial;
+};
+
+const createTwoInitials = array => {
+  const firstString = array[0];
+  const secondString = array[1];
+  const initials = `${firstString[0].toUpperCase()}${secondString[0].toUpperCase()}`;
+  return initials;
+};
+
+const hasOnlyOneValidElement = array => {
+  return !!(array[0] && !array[1] && array[0].length > 0);
+};
+
+const hasTwoValidElements = array => {
+  return !!(array[0] && array[1] && (array[0].length > 0 && array[1].length > 0));
+};
+
+const existsAndHasLength = variable => {
+  if (variable && variable.length > 0) {
+    return true;
+  }
+  return false;
 };
