fc65ef0b25ad5ddc5910f6b5e0881653940a68e6
Miki Pernu <mikael.pernu@gmail.com>
2019-06-06 14:49:26 +0300

    feat: update API calls for suggestions, remove unused ones

--- web/src/api/suggestion/suggestion.js
+++ web/src/api/suggestion/suggestion.js
@@ -1,30 +1,21 @@
 import { get, put, post, del, patch } from '../utils';
-import { asciiUriEncoding } from '../helper';
-import { sortingKeys } from '../../utils/sortingHelper';
 
 const defaultLimit = 15;
 
 export default {
-  getSuggestions: (
-    offset = 0,
-    sortValue = sortingKeys.NEWEST_FIRST,
-    filters = '',
-    searchWord = ''
-  ) =>
+  getSuggestions: (offset = 0, sort = '', filters = '', searchWord = '') =>
     get({
       // eslint-disable-next-line
-      resource: `/suggestions?limit=${defaultLimit}&offset=${offset}&sort=${sortValue}&filters=${filters}&search=${searchWord}`
+      resource: `/suggestions?limit=${defaultLimit}&offset=${offset}&sort=${sort}&filters=${filters}&search=${searchWord}`
     }),
   getSuggestionsCount: (filters, searchWord) =>
     get({ resource: `/suggestions/count?filters=${filters}&search=${searchWord}` }),
-  getSortedSuggestions: (sortValue, offset) =>
-    get({ resource: `/suggestions?limit=${defaultLimit}&sort=${sortValue}&offset=${offset}` }),
+  getSortedSuggestions: (sort, offset) =>
+    get({ resource: `/suggestions?limit=${defaultLimit}&sort=${sort}&offset=${offset}` }),
   getSuggestionsByUserId: (userId, offset = 0) =>
     get({ resource: `/suggestions/user=${userId}?limit=${defaultLimit}&offset=${offset}` }),
-  getSortedSuggestionByUserId: (userId, sortValue) =>
-    get({ resource: `/suggestions/user=${userId}?sort=${sortValue}` }),
-  getSuggestionsBySearchWord: (searchWord, offset = 0) =>
-    get({ resource: `suggestions?search=${searchWord}?limit=${defaultLimit}&offset=${offset}` }),
+  getSortedSuggestionByUserId: (userId, sort) =>
+    get({ resource: `/suggestions/user=${userId}?sort=${sort}` }),
   getSuggestionById: suggestionId => get({ resource: `/suggestions/${suggestionId}` }),
   assignUserToSuggestion: (suggestionId, userId) =>
     put({ resource: `/suggestions/${suggestionId}/assign/${userId}` }),
@@ -33,9 +24,6 @@ export default {
   assignSuggestionToMeeting: (suggestionId, meetingId) =>
     patch({ resource: `/suggestions/${suggestionId}`, data: { meeting_id: meetingId } }),
   getSuggestionByMeetingId: meetingId => get({ resource: `/suggestions/meeting/${meetingId}` }),
-  getSortedSuggestionByMeetingId: (meetingId, sortValue) =>
-    // eslint-disable-next-line
-    get({ resource: `/suggestions?sort=${sortValue}&filters=meeting_id${asciiUriEncoding.VALUE_OF_PARAM}${meetingId}` }),
   updateSuggestionStatus: (suggestionId, status) =>
     put({ resource: `/suggestions/${suggestionId}/status/${status}` }),
   addTagToSuggestion: (suggestionId, tag) =>
