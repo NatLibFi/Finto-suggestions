abde17cf43b7e1596578ae3bfd6d967cd8184d75
miguelahonen <50104570+miguelahonen@users.noreply.github.com>
2019-07-29 11:51:03 +0300

    Update tag.js
    The line starting "addNewTagStraightTo..." added to keep branches up to date and eliminate conflicts

--- web/src/api/tag/tag.js
+++ web/src/api/tag/tag.js
@@ -1,8 +1,10 @@
-import { get, del, put } from '../utils';
+import { get, del, put, post } from '../utils';
 
 export default {
   getTags: () => get({ resource: '/tags' }),
   putTag: (tagLabel, params) =>
     put({ resource: `/tags/${tagLabel}`, data: { color: params.color, label: params.label } }),
   deleteTag: tagLabel => del({ resource: `/tags/${tagLabel}` })
+  addNewTagStraightToDB: data => post({ resource: '/tags', data: data } )
 };
+
