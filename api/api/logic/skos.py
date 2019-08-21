b0ca19aca2e8f58860796ac692ee5c9c279eb728
root <root@lx8-98125-001.lib.helsinki.fi>
2019-08-21 18:50:12 +0300

    New directions with skos API, added dict handling

--- api/api/logic/skos.py
+++ api/api/logic/skos.py
@@ -1,49 +1,32 @@
-from rdflib import Graph, URIRef, Literal, Namespace, RDF, XSD, plugin
+from rdflib import Graph, URIRef, Literal, Namespace, RDF, XSD, plugin, term
 from rdflib.namespace import SKOS, DC
 from rdflib.serializer import Serializer
 import json
 import os
 import string
-#Ylläolevat toimivat ja ÄLÄ muuta näitä ellei ole ihan pakko
+from flask import jsonify
+import ast
+from .context import listContext
+#
+# from rdflib.collection import collection
+import urllib as ul
+import requests
 
-class MikasTest:
-    def toJSON(self):
-        return json.dumps(self, default=lambda o: o.__dict__, 
-            sort_keys=True, indent=4)
 
 yso = Namespace('http://www.yso.fi/onto/yso/')
 ysa = Namespace('http://www.yso.fi/onto/ysa/')
-ysemeta = Namespace("http://www.yso.fi/onto/yse-meta/")
-ysameta = Namespace("http://www.yso.fi/onto/ysa-meta/")
+ysemeta = Namespace('http://www.yso.fi/onto/yse-meta/')
+ysameta = Namespace('http://www.yso.fi/onto/ysa-meta/')
 foaf = Namespace('http://xmlns.com/foaf/0.1/')
-skos = Namespace("http://www.w3.org/2004/02/skos/core#")
+skos = Namespace('http://www.w3.org/2004/02/skos/core#')
 dct = Namespace('http://purl.org/dc/terms/')
-ysometa = Namespace("http://www.yso.fi/onto/yso-meta/")
-allars = Namespace("http://www.yso.fi/onto/allars/")
+ysometa = Namespace('http://www.yso.fi/onto/yso-meta/')
+allars = Namespace('http://www.yso.fi/onto/allars/')
 koko = Namespace('http://www.yso.fi/onto/koko/')
-
-contextInUse = {
-  "context": "http://schema.org/",
-  "type": "@type",
-  "uri": "@id",
-  "type": "@type",
-  "lang": "@language",
-  "value": "@value",
-  "graph": "@graph",
-  "label": "rdfs:label",
-  "prefLabel": "skos:prefLabel",
-  "altLabel": "skos:altLabel",
-  "hiddenLabel": "skos:hiddenLabel",
-  "broader": "skos:broader",
-  "narrower": "skos:narrower",
-  "related": "skos:related",
-  "inScheme": "skos:inScheme",
-  "exactMatch": "skos:exactMatch",
-  "closeMatch": "skos:closeMatch",
-  "broadMatch": "skos:broadMatch",
-  "narrowMatch": "skos:narrowMatch",
-  "relatedMatch": "skos:relatedMatch"
-}
+isothes = Namespace('http://purl.org/iso25964/skos-thes#')
+rdfs = Namespace('http://www.w3.org/2000/01/rdf-schema#')
+owl = Namespace('http://www.w3.org/2002/07/owl#')
+dc11 = Namespace('http://purl.org/dc/elements/1.1/')
 
 def initGraph():
   g = Graph()
@@ -52,155 +35,272 @@ def initGraph():
   g.bind('skos', skos)
   g.bind('allars', allars)
   g.bind('ysometa', ysometa)
+  g.bind('ysometa', ysometa)
   g.bind('ysameta', ysameta)
   g.bind('ysemeta', ysemeta)
   g.bind('koko', koko)
   g.bind('dc', dct)
   g.bind('foaf', foaf)
+#   print("******")
+#   print("Situation immediately after an initalization")
+#   print( g.serialize(format='json-ld', context=listContext(), indent=4).decode('utf8').replace("'", ''))
+#   print("******")
   return g
 
-def suggestionToTriple(suggestion, graph = None):
-    g = None
-    if graph is None:
-        g = initGraph()
+def codeExplicator(codeTxt: str):
+  print('\n')
+  print('****** By command: ')
+  print(codeTxt)  
+  print('******')
+
+def uriCleaner(uriToBeCleaned):
+  cleanedUri = requests.get(uriToBeCleaned).url
+  return cleanedUri
+
+def suggestionToTriple(suggestion, asJson = False, graph = None):
+    if asJson is True:
+        print("Toimii")
+        g = None
+                
+        if graph is None:
+            g = initGraph()
+        else:
+            g = graph
+
+        if suggestion is not None:
+            uri = yso['pXXX{}'.format(50000 + suggestion["id"])]
+            # g.add((uri, RDF.type, SKOS.Concept))
+            g.add((uri, RDF.type, SKOS.Concept))
+
+            for tag in suggestion["tags"]:
+                if 'maantieteellinen' in tag["label"].lower():
+                    g.add(((URIRef(uri), RDF.type, URIRef(ysometa + 'GeographicalConcept'))))
+
+            suggestion_system_url = os.environ.get('SUGGESTION_SYSTEM_ISSUE_URL', None)
+            g.add((URIRef(uri), RDF.type, URIRef(ysometa + 'ConceptAAA')))
+
+
+    # Nyt:
+    # type: [
+    # "http://finto.fi/yso/fi/ConceptAAA",
+    # "skos:ConceptQQQ"
+    # ],
+
+
+    # Pitaisi olla:
+    # "http://www.yso.fi/onto/yso-meta/Concept",
+    # "skos:Concept"
+
+
+    #OK
+            g.add((URIRef(uri),  RDF.type, URIRef(skos + 'ConceptQQQ')))
+
+            g.add((URIRef(uri), foaf.homepage, URIRef(f'{suggestion_system_url}/{suggestion["id"]}')))
+
+            #Tässä kohtaa, jos ottaa uriCleanerin pois, graph-osa häviää json-ld-tulosteesta kokonaan????
+            g.add((URIRef(uriCleaner(uri)), dct.created, Literal(suggestion["created"].date(), datatype=XSD.date)))
+
+            if "fi" in suggestion["preferred_label"].keys() and "value" in suggestion["preferred_label"]["fi"].keys():
+                g.add((URIRef(uri), skos.prefLabel, Literal(suggestion["preferred_label"]["fi"]["value"], lang='fi')))
+                
+            if "fi" in suggestion["preferred_label"].keys():
+                codeExplicator("suggestion")
+
+            if "sv" in suggestion["preferred_label"].keys() and "value" in suggestion["preferred_label"]["sv"].keys():
+                g.add((URIRef(uri), skos.prefLabel, Literal(suggestion["preferred_label"]["sv"]["value"], lang='sv')))
+
+            if "en" in suggestion["preferred_label"].keys():
+                g.add((URIRef(uri), skos.prefLabel, Literal(suggestion["preferred_label"]["en"], lang='en')))
+
+            for group in suggestion["groups"]:
+    #Toimii            g.add((URIRef(uri), skos.member, URIRef(cleanedUri)))
+                g.add((URIRef(uri), skos.member, URIRef(group["uri"])))
+                # g.add((URIRef(ul.parse.quote(uri)), skos.member, URIRef(group["uri"])))
+
+            for match in suggestion["broader_labels"]:
+                g.add((URIRef(uri), skos.broadMatch, URIRef(match["uri"])))
+
+            g.add((URIRef(uri), skos.note, Literal(suggestion["scopeNote"])))
+
+            for aLabel in suggestion["alternative_labels"]:
+                g.add((URIRef(uri), skos.altLabel, Literal(aLabel["value"]))) #)) Literal(altLabel["value"]) 
+                
+            for match in suggestion["narrower_labels"]:
+                g.add((URIRef(uri), skos.narrowMatch, URIRef(match["uri"])))
+            
+            for match in suggestion["related_labels"]:
+                print("Mitä tähän tulostuu?")
+                print(match["uri"])
+                #rdflib.term.URIRef(uri)
+                print('Entä tämä sitten?')
+                print(URIRef(match["uri"]))
+                u = URIRef(match["uri"])
+                # u = URIRef(u'www.jotain.url')
+                # g.add((URIRef(uri), skos.relatedMatch, match["uri"]))
+                #Testi g.add((URIRef(uri), skos.relatedMatch, u))
+                g.add((URIRef(uri), skos.relatedMatch, u))
+        
+        # print( g.serialize(format='json-ld', context=listContext(), indent=4).decode('utf8').replace("'", ''))
+        # print(uri)
+        # print(uriCleaner(uri))
+        # for s, p, o in g:
+        #     print((s, p, o))
+        #Periaatteessa toimiva gg = g.serialize(format='json-ld', context=listContext(), indent=4).decode('utf8').replace("'", '')
+        # gg = g.serialize(format='json-ld', context=listContext(), indent=4).decode('utf8').replace("'", '')
+        gg = g.serialize(format='json-ld', context=listContext(), indent=4).decode('utf8').replace("'", '')
+
+        
+
+        print('*** Ensin pelkkä g')
+        for s, p, o in g:
+            print('####')
+            print((s))
+            print((p))
+            print((o))
+            print('####')
+        print('*** Sitten gg')    
+        print(gg)
+
+
+        return gg
+
+
     else:
-        g = graph
+        print("Ei toimi")
 
-    if suggestion is not None:
-        uri = yso['p{}'.format(50000 + suggestion["id"])]
-        g.add((uri, RDF.type, SKOS.Concept))
+        # from functools import singledispatch
 
-        for tag in suggestion["tags"]:
-            if 'maantieteellinen' in tag["label"].lower():
-                g.add(((URIRef(uri), RDF.type, URIRef(ysameta + 'GeographicalConcept'))))
+        # @singledispatch
+        # def keys_to_strings(ob):
+        #     return ob
 
-        suggestion_system_url = os.environ.get('SUGGESTION_SYSTEM_ISSUE_URL', None)
+        # @keys_to_strings.register(dict)
+        # def _handle_dict(ob):
+        #     return {str(v): keys_to_strings(v) for k, v in ob.items()}
 
-        g.add((URIRef(uri), RDF.type, URIRef(skos + 'Concept')) )
-        g.add((URIRef(uri), RDF.type, URIRef(ysemeta + 'Concept')) )
-        g.add((URIRef(uri), foaf.homepage, URIRef(f'{suggestion_system_url}/{suggestion["id"]}')))
+        # @keys_to_strings.register(list)
+        # def _handle_list(ob):
+        #     return [keys_to_strings(v) for v in ob]
+        
+        # suggestions2 = json.dumps(_handle_list(suggestion))
+        
+        # suggestion = str(suggestion)
 
-        g.add((URIRef(uri), dct.created, Literal(suggestion["created"].date(), datatype=XSD.date)))
+                    # replaceQuotes = serialized_object.replace("'", '"')
+        # cleanedSuggestion = json.dumps(suggestion)
+            # return cleanedJson
+#         var myObj = {mykey: "my value"}
+#    ,myObjJSON = JSON.stringify(myObj);
+        # # inititialising json object 
+        # ini_string = {'nikhil': 1, 'akash' : 5,  
+        #             'manjeet' : 10, 'akshat' : 15} 
+        
+        # # printing initial json 
+        # suggestion = json.dumps(suggestion) 
+        # print ("initial 1st dictionary", suggestion) 
+        # print ("type of ini_object", type(suggestion)) 
+        
+        # # converting string to json 
+        # final_dictionary = json.loads(suggestion) 
+        
+        # # printing final result 
+        # print ("final dictionary", str(final_dictionary)) 
+        # print ("type of final_dictionary", type(final_dictionary)) 
 
-        if "fi" in suggestion["preferred_label"].keys() and "value" in suggestion["preferred_label"]["fi"].keys():
-            g.add((URIRef(uri), skos.prefLabel, Literal(suggestion["preferred_label"]["fi"]["value"], lang='fi')))
+# "\"
+# \""   
 
-        if "fi" in suggestion["preferred_label"].keys():
-            print('halloota', suggestion)
+        # data = []
+        # for line in suggestion:
+        #     data.append(json.loads(line))
 
-        if "sv" in suggestion["preferred_label"].keys() and "value" in suggestion["preferred_label"]["sv"].keys():
-            g.add((URIRef(uri), skos.prefLabel, Literal(suggestion["preferred_label"]["sv"]["value"], lang='sv')))
+        # print(data)
 
-        if "en" in suggestion["preferred_label"].keys():
-            g.add((URIRef(uri), skos.prefLabel, Literal(suggestion["preferred_label"]["en"], lang='en')))
+        # loaded_json = json.loads(str(suggestion))
+        # for x in loaded_json:
+        #     print("%s: %d" % (x, loaded_json[x]))
 
-        for group in suggestion["groups"]:
-            g.add((URIRef(uri), skos.member, URIRef(group["uri"])))
+        # class Test(object):
+        #     def __init__(self, data):
+        #         self.__dict__ = json.loads(data)
 
-        for match in suggestion["broader_labels"]:
-            g.add((URIRef(uri), skos.broadMatch, URIRef(match["uri"])))
+        # test1 = Test(suggestion)
+        # print(test1)
+        # first_json = json.dumps(suggestion)
+        # print(first_json)
 
-        g.add((URIRef(uri), skos.note, Literal(suggestion["scopeNote"])))
+        # d = {"first_name": "Alfred", "last_name":"Hitchcock"}
 
-        for label in suggestion["alternative_labels"]:
-            g.add((URIRef(uri), skos.altLabel, URIRef(label["uri"])))
-
-        for match in suggestion["narrower_labels"]:
-            g.add((URIRef(uri), skos.narrowMatch, URIRef(match["uri"])))
-
-        for match in suggestion["related_labels"]:
-            g.add((URIRef(uri), skos.relatedMatch, URIRef(match["uri"])))
-
-    #Testing via short rdf and parsing
-  #   testrdf = """
-  #   PREFIX geo: <http://www.w3.org/2003/01/geo/wgs84_pos#>
-  #   PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
-  #   PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-  #   PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-  # """
-
-  #   gg = Graph().parse(data=testrdf, format='json-ld')
-  #   ggg = (gg.serialize(format='json-ld', indent=4))
-  #   return ggg
-
-    #Testing ends
-   
-# The following is cunnently in use
-    g = g.serialize(format='turtle', context=contextInUse, indent=4).decode('utf-8')
-    # g = g.serialize(format='json-ld', indent=4)    
-    return g
+# def func(*args):
+#     mylist=[]
+#     z = {'x':(123,"SE",2,1),'q':(124,"CI",1,1)}
+#     for x,y in z.items():
+#         for t in args:
+#             if t in y:
+#                 mylist.append(x)
+#     return mylist
+# print (func(1,"CI"))
 
 
 
-# The following "works"
-# from rdflib import Graph, URIRef, Literal, Namespace, RDF, XSD
-# from rdflib.namespace import SKOS
-# import os
+        # for key,val in suggestion.items():
+        #     print("{} = {}".format(key, val))
+    
+        finalDict = {}
+        level1Dict = {}
+        level2Dict = {}
+        level3Dict = {}
+        # for x, y in suggestion:
+        #     # for t in args:
+        #     # if t in y:
+        #     # myDict.update(x, y)
+        #     # myDict.add(x, y)
+        #     myDict[x] = y
 
-# yso = Namespace('http://www.yso.fi/onto/yso/')
-# # ysa = Namespace('http://www.yso.fi/onto/ysa/')
-# ysemeta = Namespace("http://www.yso.fi/onto/yse-meta/")
-# ysameta = Namespace("http://www.yso.fi/onto/ysa-meta/")
-# foaf = Namespace('http://xmlns.com/foaf/0.1/')
-# skos = Namespace("http://www.w3.org/2004/02/skos/core#")
-# dct = Namespace('http://purl.org/dc/terms/')
+        print(type(suggestion))
+        for key in suggestion:
+            print("Avain on: ")
+            print(key)
+            print("Arvo on: ")
+            print(suggestion.get(key))
+            if type(suggestion.get(key)) is dict:
+                print("on Dikti")
+                level1Dict = suggestion.get(key)
+                for subKey1 in level1Dict:
+                    print("Sublevelillä 1 Avain on: ")
+                    print(subKey1)
+                    print("Sublevelillä 2 Arvo on: ")
+                    print(level1Dict.get(subKey1))
+            # print("Aarvo on ") + suggestion.get(key))
+            # print(suggestion.get(key))
+            # print(value)
+        # for key, value in suggestion:
+        #     myDict[key] = value
 
-# def initGraph():
-#   g = Graph()
-#   # g.bind('dc', dct)
-#   # g.bind('isothes', isothes)
-#   # g.bind('ysa', ysa)
-#   g.bind('ysa-meta', ysameta)
-#   g.bind('foaf', foaf)
-#   g.bind('skos', skos)
-#   return g
 
-# def suggestionToTriple(suggestion, graph = None):
-#   g = None
-#   if graph is None:
-#     g = initGraph()
-#   else:
-#     g = graph
+        # for key in myDict.keys:
+        #     print("Avain on: " + key)
+        #     print("Arvo on: " + myDict.get(key))
+    # print (func(1,"CI"))
 
-#   if suggestion is not None:
-#     uri = yso['p{}'.format(50000 + suggestion.id)]
-#     g.add((uri, RDF.type, SKOS.Concept))
 
-#     for tag in suggestion.tags:
-#       if 'maantieteellinen' in tag.label.lower():
-#         g.add(((URIRef(uri), RDF.type, URIRef(ysameta + 'GeographicalConcept'))))
+# current_dict = {'corse': 378, 'cielo': 209, 'mute': 16}
+# print(current_dict)
+# def replace_value_with_definition(key_to_find, definition):
+#     for key in current_dict.keys():
+#         if key == key_to_find:
+#             current_dict[key] = definition
 
-#     suggestion_system_url = os.environ.get('SUGGESTION_SYSTEM_ISSUE_URL', None)
+# replace_value_with_definition('corse', 'Definition of "corse"')
+# print(current_dict)
 
-#     g.add((URIRef(uri), RDF.type, URIRef(skos + 'Concept')) )
-#     g.add((URIRef(uri), RDF.type, URIRef(ysemeta + 'Concept')) )
-#     g.add((URIRef(uri), foaf.homepage, URIRef(f'{suggestion_system_url}/{suggestion.id}')))
-#     g.add((URIRef(uri), dct.created, Literal(suggestion.created.date(), datatype=XSD.date)))
 
-#     if suggestion.preferred_label.fi.value:
-#       g.add((URIRef(uri), skos.prefLabel, Literal(suggestion.preferred_label.fi.value, lang='fi')))
 
-#     if suggestion.preferred_label.sv.value:
-#       g.add((URIRef(uri), skos.prefLabel, Literal(suggestion.preferred_label.sv.value, lang='sv')))
 
-#     if suggestion.preferred_label.en:
-#       g.add((URIRef(uri), skos.prefLabel, Literal(suggestion.preferred_label.en, lang='en')))
+        return suggestion
 
-#     for group in suggestion.groups:
-#       g.add((URIRef(uri), skos.member, URIRef(group.uri)))
 
-#     for match in suggestion.broader_labels:
-#       g.add((URIRef(uri), skos.broadMatch, URIRef(match.uri)))
 
-#     g.add((URIRef(uri), skos.note, suggestion.scopeNote))
-#     # g.add((URIRef(uri), skos.note, 'Mikas testing'))
 
-#     for label in suggestion.alternative_labels:
-#       g.add((URIRef(uri), skos.altLabel, URIRef(label.uri)))
 
-#     for match in suggestion.narrower_labels:
-#       g.add((URIRef(uri), skos.narrowMatch, URIRef(match.uri)))
-
-#     for match in suggestion.related_labels:
-#       g.add((URIRef(uri), skos.relatedMatch, URIRef(match.uri)))
 
--- api/api/logic/suggestions.py
+++ api/api/logic/suggestions.py
@@ -14,6 +14,7 @@ from rdflib import Graph, URIRef, Literal, Namespace, RDF
 from rdflib.namespace import SKOS
 
 from ..tools.profiler import profiler
+import json
 
 # Profiler decorator, enable if needed
 # @profiler
@@ -426,11 +427,39 @@ def get_suggestion_skos(suggestion_id: int) -> str:
     :returns: A single suggestion object as json
     """
 
+    # try:
+    #     suggestion = Suggestion.query.filter_by(id=suggestion_id).first()
+    #     # serialized_object = suggestion.as_dict()
+    #     serialized_object = suggestionToTriple(suggestion.as_dict() )
+    #     # return { 'data': serialized_object, 'code': 200 }, 200
+    #     replaceQueotes = serialized_object.replace("", '"')
+    #     cleanedJson = json.loads(replaceQueotes)
+    #     return cleanedJson
+    #     # return serialized_object
+    # except Exception as ex:
+    #     print(str(ex))
+    #     return { 'code': 404, 'error': str(ex) }, 404
+
+
+
     try:
         suggestion = Suggestion.query.filter_by(id=suggestion_id).first()
         # serialized_object = suggestion.as_dict()
-        serialized_object = suggestionToTriple(suggestion.as_dict())
-        return { 'data': serialized_object, 'code': 200 }, 200
+        prms = {"asJson" : False}
+        serialized_object = suggestionToTriple(suggestion.as_dict(), **prms )
+        # suggestion.asJson
+        # serialized_object = suggestionToTriple(suggestion, **prms )
+        # return { 'data': serialized_object, 'code': 200 }, 200
+        # replaceQueotes = serialized_object.replace("", '"')
+        if prms["asJson"]:
+        # if prms is True:
+            print("Jäätiin iffiin")
+            replaceQuotes = serialized_object.replace("'", '"')
+            cleanedJson = json.loads(replaceQuotes)
+            return cleanedJson
+        else: 
+            print("Jäätiin elseen")
+            return serialized_object
     except Exception as ex:
         print(str(ex))
         return { 'code': 404, 'error': str(ex) }, 404
