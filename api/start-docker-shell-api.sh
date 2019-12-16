459624d727d30df533ceaa33d89fa63db37cd7cb
miguelahonen <50104570+miguelahonen@users.noreply.github.com>
2019-12-16 16:30:50 +0200

    Develop (#220)
    * Now installing packages with Pipenv.lock explicitly shows installed packages and versions
    
    * Improved gunicorn implementation (#218)
    
    * Improved gunicorn implementation (#219)

--- api/start-docker-shell-api.sh
+++ api/start-docker-shell-api.sh
@@ -4,6 +4,7 @@ read -p "You are entering the Docker Shell - if ok press 'y': " answer
 case ${answer:0:1} in
     y|Y )
         docker exec -it finto-suggestions_api_1 sh
+        # docker exec -it 8b4e6113eb34_finto-suggestions_api_1 sh
     ;;
     * )
         echo "Maybe next time.."
