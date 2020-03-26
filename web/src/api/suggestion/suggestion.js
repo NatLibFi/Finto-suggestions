421e63b6babdc264b6c8d83755042447874f60b2
miguelahonen <mika.ahonen@helsinki.fi>
2020-03-26 16:43:36 +0200

    Tarpeellisia pieniä säätöjä

--- web/src/api/suggestion/suggestion.js
+++ web/src/api/suggestion/suggestion.js
@@ -5,10 +5,10 @@ import { get, put, post, del, patch } from '../utils';
 const defaultLimit = 25;
 
 export default {
-  getSuggestions: (offset = 0, sort = '', filters = '', searchWord = '') =>
+  getSuggestions: (offset = 0, sort = '', filters = '', searchWord = '', areaTerm = '') =>
     get({
       // eslint-disable-next-line max-len
-      resource: `/suggestions?limit=${defaultLimit}&offset=${offset}&sort=${sort}&filters=${filters}&search=${searchWord}`
+      resource: `/suggestions?limit=${defaultLimit}&offset=${offset}&sort=${sort}&filters=${filters}&search=${searchWord}&area=${areaTerm}`
     }),
   getSuggestionsCount: (filters, searchWord) =>
     get({ resource: `/suggestions/count?filters=${filters}&search=${searchWord}` }),
